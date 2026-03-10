9a444d1cb51a5c5d5ac19c155df94942b086e53b
dickyyahya <dickyyahya25@gmail.com>
2026-03-10 21:34:37 +0700

    tambah kondisi login.php

--- login.php
+++ login.php
@@ -6,16 +6,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login | Warung Savon</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
 </head>
 
-<body>
+<body id="bg-login">
     <div class="box-login">
         <h2>login</h2>
         <form action="" method="POST">
-            <input type="text" name="user" placeholder="Username">
-            <input type="password" name="pass" placeholder="Password">
-            <input type="submit" name="submit" value="Login">
+            <input type="text" name="user" placeholder="Username" class="input-control">
+            <input type="password" name="pass" placeholder="Password" class="input-control">
+            <input type="submit" name="submit" value="Login" class="btn">
         </form>
+        <?php
+        if (isset($_POST['submit'])) {
+            include 'db.php';
+
+            $user = $_POST['user'];
+            $pass = $_POST['pass'];
+
+            $cek = mysqli_query($conn, "SELECT * FROM tb_admin WHERE username = '" . $user . "' AND password = '" . MD5($pass) . "'");
+            echo mysqli_num_rows($cek);
+        }
+        ?>
     </div>
 </body>
 
