b9e37921624c76d8ec5a6d9a58fa8b84aa26c1b1
dickyyahya <dickyyahya25@gmail.com>
2026-03-10 20:52:21 +0700

    tambah login.php

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login | Warung Savon</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="box-login">
+        <h2>login</h2>
+        <form action="" method="POST">
+            <input type="text" name="user" placeholder="Username">
+            <input type="password" name="pass" placeholder="Password">
+            <input type="submit" name="submit" value="Login">
+        </form>
+    </div>
+</body>
+
+</html>
